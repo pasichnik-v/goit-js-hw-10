108de3439637238e97c4415e91dc04bea185823f
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-03-07 22:57:06 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+
+text = console.log('Привіт, Ярослав');
